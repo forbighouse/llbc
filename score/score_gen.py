12c2b8776e20803c4f263fb022521b8b2e8d3824
bighouse <forbighouse@163.com>
2019-01-03 18:08:27 +0800

    init commit

---
+++ score/score_gen.py
@@ -0,0 +1,15 @@
+
+
+class Score():
+
+    def __init__(self):
+        # Todo
+        pass
+
+    def gen_score(self):
+        # Todo
+        pass
+
+    @staticmethod
+    def c_score_test():
+        print ("This is the c_score_test test file, it's import successed")
---
+++ test_init.py
@@ -0,0 +1,12 @@
+from score.score_gen import Score
+
+
+def test():
+
+    print ("This is the test function")
+    a = Score()
+    a.c_score_test()
+
+
+if __name__ == "__main__":
+    test()
