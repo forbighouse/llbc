4d2257419a36070ed02ecaa63456948843b89894
forbighouse <forbighouse@163.com>
2019-01-10 18:51:44 +0800

    commit 4

--- score/observation.py
+++ score/observation.py
@@ -1,6 +1,12 @@
 
 
 class Observation(object):
+
+    # zone: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    # section: a, b, c, d
+    # event: accident, light
+    # time: 1, 2, 3
+
     fields = [
         ('observe_time',    int),
         ('observe_zone',    str),
@@ -15,11 +21,14 @@ class Observation(object):
                  observe_event='accident'):
         fields = {k: v for k, v in locals().items() if k not in ['self', '__class__']}
         fields['observe_time'] = 111
-        print(fields['observe_event'])
-
-
-
-        # print(fields)
+        if not isinstance(observe_time, int):
+            raise TypeError('bad observe_time input, it should be int')
+        if not isinstance(observe_zone, str):
+            raise TypeError('bad observe_zone input, it should be str')
+        if not isinstance(observe_section, str):
+            raise TypeError('bad observe_section input, it should be str')
+        if not isinstance(observe_event, str):  # 以后要改成固定的几个选项
+            raise TypeError('bad observe_event input, it should be str')
 
 # def set_observation(obs):
 #     ob1 = Observation()
--- test_script/observation_test.py
+++ test_script/observation_test.py
@@ -3,12 +3,7 @@ from score.observation import Observation
 
 def test_ob():
 
-    '''
-    zone: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    section: a, b, c, d
-    event: accident, light
-    time: 1, 2, 3
-    '''
+
     exam_obs = [
         {'zone': 0, ' section': 'a', 'event': 'accident', 'time': 1},
         {'zone': 2, ' section': 'b', 'event': 'light', 'time': 2},
@@ -17,6 +12,9 @@ def test_ob():
         {'zone': 4, ' section': 'c', 'event': 'accident', 'time': 2},
         {'zone': 7, ' section': 'd', 'event': 'accident', 'time': 3},
     ]
+    # 位置txt
+    # 10分钟
+
 
     a = Observation()
     b = Observation(000, '000', '111', 'light')
