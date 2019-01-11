c576544c02026201a4464e4c93e9e472f4b9191a
forbighouse <forbighouse@163.com>
2019-01-11 17:44:04 +0800

    commit 4

--- test_script/observation_test.py
+++ test_script/observation_test.py
@@ -1,24 +1,32 @@
 from score.observation import Observation
-
+from score.nodemanager import ServiceNode
 
 def test_ob():
-
-
-    exam_obs = [
-        {'zone': 0, ' section': 'a', 'event': 'accident', 'time': 1},
-        {'zone': 2, ' section': 'b', 'event': 'light', 'time': 2},
-        {'zone': 2, ' section': 'd', 'event': 'accident', 'time': 1},
-        {'zone': 1, ' section': 'a', 'event': 'light', 'time': 2},
-        {'zone': 4, ' section': 'c', 'event': 'accident', 'time': 2},
-        {'zone': 7, ' section': 'd', 'event': 'accident', 'time': 3},
-    ]
+    # exam_obs = [
+    #     {'zone': 0, ' section': 'a', 'event': 'accident', 'time': 1},
+    #     {'zone': 2, ' section': 'b', 'event': 'light', 'time': 2},
+    #     {'zone': 2, ' section': 'd', 'event': 'accident', 'time': 1},
+    #     {'zone': 1, ' section': 'a', 'event': 'light', 'time': 2},
+    #     {'zone': 4, ' section': 'c', 'event': 'accident', 'time': 2},
+    #     {'zone': 7, ' section': 'd', 'event': 'accident', 'time': 3},
+    # ]
     # 位置txt
     # 10分钟
+    ServiceNode(node_type=1, veh_id='001', longitude=123, latitude=321, observe_vehs=[], current_time=12)
+    # veh_traj = [
+    #     {'veh_id': 0, 'longitude': 0, 'latitude': 0, 'observe_vehs': 'accident', 'current_time': 1},
+    #     {'zone': 2, ' section': 'b', 'event': 'light', 'time': 2},
+    #     {'zone': 2, ' section': 'd', 'event': 'accident', 'time': 1},
+    #     {'zone': 1, ' section': 'a', 'event': 'light', 'time': 2},
+    #     {'zone': 4, ' section': 'c', 'event': 'accident', 'time': 2},
+    #     {'zone': 7, ' section': 'd', 'event': 'accident', 'time': 3},
+    # ]
+    ServiceNode.showfield()
 
-
-    a = Observation()
-    b = Observation(000, '000', '111', 'light')
-    print()
+    #
+    # a = Observation()
+    # b = Observation(000, '000', '111', 'light')
+    # print()
 
 
 if __name__ == '__main__':
